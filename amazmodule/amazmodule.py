fde7355804738440bf75aedef069c26604d760f3
devin shields <devin.shields@gmail.com>
2016-01-18 14:42:46 -0500

    added a nice failing test

--- amazmodule/amazmodule.py
+++ amazmodule/amazmodule.py
@@ -1,15 +1,19 @@
 #!/usr/bin/env python
 '''  '''
 
-def string_repeater(any_string):
+def string_repeater(input_string):
     ''' function repeats any string you throw at this baby. twice. '''
-    return any_string + any_string
+    if not isinstance(input_string, basestring):
+        raise TypeError('input_string must be of string type!')
+    if not len(input_string):
+        raise ValueError("You can't repeat an empty string, you silly goose!")
+    return input_string + input_string
 
 
 def main():
-  some_string = 'Goooooooooooooooooooood morning vietnam!'
-  repeated_string = string_repeater(some_string)
-  print '\nsome_string: {}\nrepeated_string: {}\n'.format(some_string, repeated_string)
+  input = 'Goooooooooooooooooooood morning vietnam!'
+  output = string_repeater(input)
+  print '\ninput:  {}\noutput: {}\n'.format(input, output)
 
 
 if __name__ == '__main__':
---
+++ tests/test_amazmodule.py
@@ -0,0 +1,22 @@
+
+from nose.tools import assert_raises
+
+import amazmodule
+
+
+def test_string_repeater():
+    input = 'hello'
+    ouput = amazmodule.string_repeater(input)
+    assert ouput == 'hellohello'
+
+
+def test_string_repeater_wrong_type():
+    assert_raises(TypeError, amazmodule.string_repeater, 3)
+
+
+def test_string_repeater_too_short():
+    #assert_raises(ValueError, amazmodule.string_repeater, 3)
+
+    # BOGUS/BROKEN TEST
+    assert amazmodule.string_repeater('') == ''
+
