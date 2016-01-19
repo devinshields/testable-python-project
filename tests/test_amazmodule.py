3bbb4e4f89d72546a97709edc260897adf19ae23
devin shields <devinshields@users.noreply.github.com>
2016-01-18 20:55:28 -0500

    Update test_amazmodule.py

--- tests/test_amazmodule.py
+++ tests/test_amazmodule.py
@@ -9,14 +9,8 @@ def test_string_repeater():
     ouput = amazmodule.string_repeater(input)
     assert ouput == 'hellohello'
 
-
 def test_string_repeater_wrong_type():
     assert_raises(TypeError, amazmodule.string_repeater, 3)
 
-
 def test_string_repeater_too_short():
-    #assert_raises(ValueError, amazmodule.string_repeater, 3)
-
-    # BOGUS/BROKEN TEST
-    assert amazmodule.string_repeater('') == ''
-
+    assert_raises(ValueError, amazmodule.string_repeater, '')
